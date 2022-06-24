9d5bc72c5f7d79347e5ff26de2a50b370edac113
Esteban Restrepo <erestrepo@webcol.net>
2022-06-23 19:33:42 -0500

    se añade bootstrap

--- administrador/index.php
+++ administrador/index.php
@@ -1,16 +1,35 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Administrador del sitio web</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
 </head>
 
 <body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
 
+                <div class="card">
+                    <div class="card-header">
+                        header
+                    </div>
+                    <div class="card-body">
+                        <h4 class="card-title"></h4>
+                        <p class="card-text">text</p>
+                    </div>
+                </div>
+                <div class="card-footer text-muted">
+                    footer
+                </div>
 
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
